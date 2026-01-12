de4cef2b8547f443527970139dbc1321c72580c3
Dmitry Utropov <dmitry.wu@gmail.com>
2026-01-12 04:36:56 +0300

    add urfave v3 lib

--- Makefile
+++ Makefile
@@ -3,3 +3,6 @@ BUILD_DIR=bin
 
 build:
 	go build -o $(BUILD_DIR)/$(BINARY_NAME) ./cmd/hexlet-path-size
+
+run:
+	go run ./cmd/hexlet-path-size
--- cmd/hexlet-path-size/main.go
+++ cmd/hexlet-path-size/main.go
@@ -1,10 +1,25 @@
 ﻿package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/urfave/cli/v3"
+)
 
 func main() {
-	fmt.Print("\033[H\033[2J")
+	app := &cli.Command{
+		Name:  "hexlet-path-size",
+		Usage: "Prints size of a directory or file",
+		Action: func(ctx context.Context, cmd *cli.Command) error {
+			fmt.Println("Hello from Hexlet!")
+			return nil
+		},
+	}
 
-	fmt.Println("Hello from Hexlet!")
-
+	if err := app.Run(context.Background(), os.Args); err != nil {
+		log.Fatal(err)
+	}
 }
